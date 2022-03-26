d72a1deb86f1dbffddb9d839d3f2fdae56d59cd0
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-26 20:01:38 -0300

    12.7. Introdução ao Grid (Row e Col)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { DatePicker } from 'antd';
+import { Col, Row } from 'antd';
+
+import './index.css';
 
 function App() {
   return (
-    <div className='App'>
-      <DatePicker />
+    <div>
+      <Row>
+        <Col className='Column' span={4}>
+          Coluna 1
+        </Col>
+        <Col className='Column' span={4}>
+          Coluna 2
+        </Col>
+        <Col className='Column' span={8}>
+          Coluna 3
+        </Col>
+        <Col className='Column' span={4}>
+          Coluna 4
+        </Col>
+        <Col className='Column' span={6}>
+          Coluna 5
+        </Col>
+      </Row>
     </div>
   );
 }
