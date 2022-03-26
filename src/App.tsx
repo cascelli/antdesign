03bb430db25ee98782a7792bd7d936b32a117f94
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-26 20:36:30 -0300

    12.9. Responsividade com Breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ function App() {
   return (
     <div>
       <Row gutter={8} justify={'center'} align={'middle'}>
-        <Col span={4}>
+        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
           <div className='Column'>Coluna 1</div>
         </Col>
+        {/*         
         <Col span={4}>
           <div className='Column'>Coluna 2</div>
         </Col>
@@ -21,6 +22,7 @@ function App() {
         <Col span={6}>
           <div className='Column'>Coluna 5</div>
         </Col>
+        */}
       </Row>
     </div>
   );
