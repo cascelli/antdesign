5afeb1619014fd9846864c0fb84c4bbea8c9f1fa
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-27 10:41:04 -0300

    12.14. Introdução ao Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,58 @@
-import { Col, Input, Row, Table } from 'antd';
-import { User, UserService } from 'danielbonifacio-sdk';
-import { useEffect, useState } from 'react';
+import { Button, Col, Form, Input, Row } from 'antd';
 
 import './index.css';
 
 function App() {
-  const [name, setName] = useState('');
-  const [users, setUsers] = useState<User.Summary[]>([]);
-
-  useEffect(() => {
-    UserService.getAllUsers().then(setUsers);
-  }, []);
-
   return (
     <div>
-      <Row gutter={16}>
-        <Col span={4}>{name}</Col>
-        <Col span={12}>
-          <Input
-            placeholder={'E.g.: Julio Costa'}
-            size={'small'}
-            addonBefore='https://'
-            prefix='subdomain'
-            addonAfter='.com.br'
-            suffix='empresa'
-            onChange={(e) => {
-              setName(e.currentTarget.value);
-            }}
-          />
-          <Input.TextArea
-            placeholder={'E.g.: Julio Costa'}
-            size={'small'}
-            onChange={(e) => {
-              setName(e.currentTarget.value);
-            }}
-          />
-        </Col>
-        <Col span={24}>
-          <Table
-            dataSource={users}
-            columns={[
-              {
-                dataIndex: 'id',
-                title: 'ID',
-              },
-              {
-                dataIndex: 'name',
-                title: 'Nome',
-              },
-              {
-                dataIndex: 'email',
-                title: 'Email',
-              },
-            ]}
-          />
-        </Col>
-      </Row>
+      <Form
+        layout={'vertical'}
+        autoComplete={'off'}
+        onFinish={(form) => {
+          // console.log('Envir formulário');
+          console.log(form);
+        }}
+      >
+        <Row gutter={16}>
+          <Col span={8}>
+            <Form.Item
+              label={'Primeiro nome'}
+              name={'firstName'}
+            >
+              <Input placeholder={'Ex.: João'} />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              label={'Último nome'}
+              name={'lastName'}
+            >
+              <Input placeholder={'Ex.: Batista'} />
+            </Form.Item>
+          </Col>
+          <Col span={16}>
+            <Form.Item
+              label={'E-mail'}
+              name={'email'}
+              rules={[
+                {
+                  required: true,
+                  message: 'E-mail é obrigatório',
+                },
+              ]}
+            >
+              <Input
+                placeholder={'Ex.: joao.batista@gmail.com'}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Button type={'primary'} htmlType={'submit'}>
+              Enviar dados
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </div>
   );
 }
