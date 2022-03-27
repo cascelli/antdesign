86ffeb0aee605052d0c69233d59ccdf45170dd37
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-26 21:18:50 -0300

    12.10. Tipografia

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import { Col, Row } from 'antd';
-
+import { Col, Row, Typography } from 'antd';
+import { useState } from 'react';
 import './index.css';
 
+const { Title, Paragraph } = Typography;
+
 function App() {
+  const [str, setStr] = useState('Daniel Bonifacio');
+
   return (
     <div>
-      <Row gutter={8} justify={'center'} align={'middle'}>
-        <Col xs={24} sm={16} md={12} lg={8} xl={4} xxl={2}>
-          <div className='Column'>Coluna 1</div>
+      <Row gutter={8} justify={'center'}>
+        <Col span={12}>
+          <Title>Título de nível 1</Title>
+          <Title level={2}>Título de nível 2</Title>
+          <Title level={3}>Título de nível 3</Title>
+          <Title level={4}>Título de nível 4</Title>
+          <Title level={5}>Título de nível 5</Title>
         </Col>
-        {/*         
-        <Col span={4}>
-          <div className='Column'>Coluna 2</div>
+
+        <Col span={12}>
+          <Paragraph editable={{ onChange: setStr }}>
+            {str}
+          </Paragraph>
+          <Paragraph ellipsis={{ rows: 2 }}>
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. In neque quas ratione.
+            Suscipit repudiandae mollitia rem quae id?
+            Voluptate pariatur quisquam eveniet debitis
+            reprehenderit, doloribus assumenda laborum quas
+            sapiente corporis!
+          </Paragraph>
         </Col>
-        <Col span={8}>
-          <div className='Column'>Coluna 3</div>
-        </Col>
-        <Col span={4}>
-          <div className='Column'>Coluna 4</div>
-        </Col>
-        <Col span={6}>
-          <div className='Column'>Coluna 5</div>
-        </Col>
-        */}
       </Row>
     </div>
   );
