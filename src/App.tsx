74261976dbd1870f1589c880bf2895c15733c18d
Marcos Antonio Cascelli <marcos.cascelli@gmail.com>
2022-03-27 10:04:23 -0300

    12.13. Introdução ao Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import { Col, Row, Table } from 'antd';
+import { Col, Input, Row, Table } from 'antd';
 import { User, UserService } from 'danielbonifacio-sdk';
 import { useEffect, useState } from 'react';
 
 import './index.css';
 
 function App() {
+  const [name, setName] = useState('');
   const [users, setUsers] = useState<User.Summary[]>([]);
+
   useEffect(() => {
     UserService.getAllUsers().then(setUsers);
   }, []);
 
   return (
     <div>
-      <Row gutter={8} justify={'center'}>
+      <Row gutter={16}>
+        <Col span={4}>{name}</Col>
+        <Col span={12}>
+          <Input
+            placeholder={'E.g.: Julio Costa'}
+            size={'small'}
+            addonBefore='https://'
+            prefix='subdomain'
+            addonAfter='.com.br'
+            suffix='empresa'
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+            }}
+          />
+          <Input.TextArea
+            placeholder={'E.g.: Julio Costa'}
+            size={'small'}
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+            }}
+          />
+        </Col>
         <Col span={24}>
           <Table
             dataSource={users}
             columns={[
-              { dataIndex: 'id', title: 'ID' },
-              { dataIndex: 'name', title: 'Nome' },
-              { dataIndex: 'email', title: 'E-mail' },
+              {
+                dataIndex: 'id',
+                title: 'ID',
+              },
+              {
+                dataIndex: 'name',
+                title: 'Nome',
+              },
+              {
+                dataIndex: 'email',
+                title: 'Email',
+              },
             ]}
           />
         </Col>
